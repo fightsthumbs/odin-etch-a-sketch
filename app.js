361133fe2f28e098b387f6c22cf0c8f1254a9920
Benjamín Álvarez Rodríguez <fightsthumbs@gmail.com>
2022-06-12 04:46:49 -0600

    add slider to select canvas size

--- app.js
+++ app.js
@@ -6,19 +6,42 @@ const canvas = document.getElementById('canvas');
 
 const rangeNum = document.getElementById('rangeNum');
 const rangeGridNum = document.getElementById('range-grid-num');
+let gridNum = 3;
 
 rangeGridNum.addEventListener("change",()=> {
   console.log(rangeGridNum.value);
   rangeNum.textContent = rangeGridNum.value
+  gridNum = rangeGridNum.value
+  createCanvas();
 })
 
-
-const gridNum = 16;
 //   }
 // }
+createCanvas();
 
-for (let i = 0; i<(gridNum*gridNum); i++) {
-  const squares = document.createElement("div")
-  canvas.appendChild(squares);
+
+function createCanvas() {
+  const actualCanvas = document.querySelectorAll(".drawing-div");
+  console.log(actualCanvas)
+  actualCanvas.forEach(i => {
+    canvas.removeChild(i);
+  })
+  
+  for (let i = 0; i<(gridNum*gridNum); i++) {
+    const individualSquare = document.createElement("div");
+    individualSquare.setAttribute('class', 'drawing-div');  
+    canvas.appendChild(individualSquare);
+  }
+  addEventsToDiv()
+}
+
+
+
+function addEventsToDiv() {
+  const squares = document.querySelectorAll(".drawing-div");
+  squares.forEach(element => element.addEventListener("mouseover", (e) => {
+  
+    e.target.classList.toggle("color-div")
+  }))
 
 }
